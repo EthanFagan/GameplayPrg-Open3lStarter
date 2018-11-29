f5c141a74956829166f1933c2445b8740bc91935
Ethan Fagan <C00232277@itcarlow.ie>
2018-11-29 12:58:19 +0000

    nez

--- MuddyGames-gameplay-programming-i-lab-05-cc544c1acdcd/SFMLOpenGL/Game.cpp
+++ MuddyGames-gameplay-programming-i-lab-05-cc544c1acdcd/SFMLOpenGL/Game.cpp
@@ -39,7 +39,7 @@ void Game::initialize()
 
 	
 
-	glClearColor(255.0f, 255.0f, 255.0f, 0.0f);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluPerspective(45.0, window.getSize().x / window.getSize().y, 1.0, 500.0);
@@ -55,7 +55,7 @@ void Game::initialize()
 	glEndList();
 
 	glNewList(index + 1, GL_COMPILE);
-	glPointSize((10.0f));
+	glPointSize((5.0f));
 	glColor3f(255.0f, 0.0f, 0.0f);
 	glBegin(GL_POINTS);
 	{
@@ -78,7 +78,7 @@ void Game::initialize()
 	glEndList();
 
 	glNewList(index + 3, GL_COMPILE);
-	glColor3f(0.0f, 0.0f, 0.0f);
+	glColor3f(255.0f, 255.0f, 255.0f);
 	glBegin(GL_LINE_STRIP);
 	{
 		glVertex3f(0.0, 0.0, -5.0);
@@ -90,7 +90,7 @@ void Game::initialize()
 	glEndList();
 
 	glNewList(index + 4, GL_COMPILE);
-	glColor3f(0.0f, 0.0f, 0.0f);
+	glColor3f(255.0f, 255.0f, 255.0f);
 	glBegin(GL_LINE_LOOP);
 	{
 		glVertex3f(0.0, 0.0, -5.0);
@@ -114,7 +114,7 @@ void Game::initialize()
 	glEndList();
 
 	glNewList(index + 6, GL_COMPILE);
-	glColor3f(0.0, 0.0, 0.0);
+	glColor3f(255.0, 255.0, 0.0);
 	glBegin(GL_TRIANGLE_FAN);
 	{
 		glVertex3f(0.0, 1.0, -10.5);
@@ -205,135 +205,6 @@ void Game::update()
 
 void Game::draw()
 {
-	/*cout << "Draw up" << endl;
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	if (timer < 300)
-	{
-		glColor3f(0.0f, 255.0f, 0.0f);
-		glBegin(GL_TRIANGLES);
-		{ glVertex3f(0.0, 2.0, -5.0);
-		glVertex3f(-2.0, -2.0, -5.0);
-		glVertex3f(2.0, -2.0, -5.0); }
-		glEnd();
-	}
-	else if (timer < 600)
-	{
-		glPointSize((10.0f));
-		glColor3f(255.0f, 0.0f, 0.0f);
-		glBegin(GL_POINTS);
-		{
-			glVertex3f(0.0, 2.0, -5.0);
-			glVertex3f(-2.0, -2.0, -5.0);
-			glVertex3f(2.0, -2.0, -5.0);
-		}
-		glEnd();
-	}
-
-	
-	else if (timer < 900)
-	{
-		glLineWidth(5.0f);
-		glColor3f(255.0f, 255.0f, 0.0f);
-		glBegin(GL_LINES);
-		{
-			glVertex3f(1.0, 0.0, -5.0);
-			glVertex3f(-2.0, -2.0, -5.0);
-		}
-		glEnd();
-	}
-	
-	else if (timer < 1200)
-	{
-		glColor3f(0.0f, 0.0f, 0.0f);
-		glBegin(GL_LINE_STRIP);
-		{
-			glVertex3f(0.0, 0.0, -5.0);
-			glVertex3f(-2.0, -2.0, -5.0);
-			glVertex3f(2.0, 1.5, -6.0);
-			glVertex3f(1.0, 0.0, -3.0);
-		}
-		glEnd();
-	}
-	else if (timer < 1500)
-	{
-		glColor3f(0.0f, 0.0f, 0.0f);
-		glBegin(GL_LINE_LOOP);
-		{
-			glVertex3f(0.0, 0.0, -5.0);
-			glVertex3f(-2.0, -2.0, -5.0);
-			glVertex3f(2.0, 1.5, -6.0);
-			glVertex3f(1.0, 0.0, -3.0);
-		}
-		glEnd();
-	}
-	else if (timer < 1800)
-	{
-		glColor3f(255.0f, 0.0f, 255.0f);
-		glBegin(GL_TRIANGLE_STRIP);
-		{
-			glVertex3f(0.0, 0.0, -10.5);
-			glVertex3f(-1.0, -1.0, -10.5);
-			glVertex3f(1.0, -1.0, -10.5);
-			glVertex3f(2.0, -2.0, -10.5);
-		}
-		glEnd();
-	}
-	else if (timer < 2100)
-	{
-		glColor3f(0.0, 0.0, 0.0);
-		glBegin(GL_TRIANGLE_FAN);
-		{
-			glVertex3f(0.0, 1.0, -10.5);
-			glVertex3f(-1.0, -1.0, -10.5);
-			glVertex3f(1.0, -1.0, -10.5);
-			glVertex3f(2.0, -2.0, -10.5);
-			glVertex3f(-1.0, -2.0, -10.5);
-		}
-		glEnd();
-	}
-	else if (timer < 2400)
-	{
-		glColor3f(110.0, 0.0, 60.0);
-		glBegin(GL_QUADS);
-		{
-			glVertex3f(2.0, -2.0, -10.0);
-			glVertex3f(2.0, 2.0, -10.0);
-			glVertex3f(-2.0, 2.0, -10.0);
-			glVertex3f(-2.0, -2.0, -10.0);
-		}
-		glEnd();
-	}
-	else if (timer < 2700)
-	{
-		glColor3f(255.0, 0.0, 0.0);
-		glBegin(GL_QUAD_STRIP);
-		{
-			glVertex3f(2.0, -2.0, -10.0);
-			glVertex3f(2.0, 2.0, -10.0);
-			glVertex3f(-2.0, -2.0, -10.0);
-			glVertex3f(-2.0, 2.0, -10.0);
-			glVertex3f(-3.0, -2.0, -10.0);
-			glVertex3f(-3.0, 2.0, -10.0);
-		}
-		glEnd();
-	}
-	else if (timer < 3000)
-	{
-		glColor3f(0.0,0.0,255.0);
-		glBegin(GL_POLYGON);
-		{
-			glVertex3f(0.0, 0.0, -20.0);
-			glVertex3f(0.0, 3.0, -20.0);
-			glVertex3f(4.0, 3.0, -20.0);
-			glVertex3f(6.0, 1.5, -20.0);
-			glVertex3f(4.0, 0.0, -20.0);
-		}
-		glEnd();
-	}
-	else if (timer > 3000)
-	{
-		timer = 0;
-	}*/
 
 	cout << "Draw up" << endl;
 
